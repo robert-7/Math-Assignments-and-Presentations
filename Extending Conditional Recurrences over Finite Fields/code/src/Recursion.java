7bb1257f7c59de68871df6da2f0bdbc21f0a2325
Robert Lech <robert.lech123@gmail.com>
2023-09-22 10:33:58 -0400

    ci: migrates from Travis CI to GitHub actions (#12)
    * ci: adds github actions
    
    * ci: adds yamllint
    
    * ci: adds trailing whitespace linter and java cleanup
    
    * ci: adds end-of-file-fixer linter
    
    * ci: adds chktex linting
    
    * ci: fixes chktex compatibility
    
    * ci: adds python linting
    
    * chore: cleans up repo
    
    * ci: removes travis.yml

--- Extending Conditional Recurrences over Finite Fields/code/src/Recursion.java
+++ Extending Conditional Recurrences over Finite Fields/code/src/Recursion.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 
 /**
@@ -8,132 +8,244 @@
  */
 public class Recursion {
 
-	int[] ics;
-	int[][] coefs;
-	int field;
+    int[] ics;
+    int[][] coefs;
+    int field;
 
-	public Recursion(int[] ic, int[][] coefs, int field) {
-		this.ics = ic;
-		this.coefs = coefs;
-		this.field = field;
-	}
-	
-	public Recursion(int[] ic, int[][] coefs) {
-		this.ics = ic;
-		this.coefs = coefs;
-		this.field = Integer.MAX_VALUE;
-	}
+    public Recursion(int[] ic, int[][] coefs, int field) {
+        this.ics = ic;
+        this.coefs = coefs;
+        this.field = field;
+    }
+
+    public Recursion(int[] ic, int[][] coefs) {
+        this.ics = ic;
+        this.coefs = coefs;
+        this.field = Integer.MAX_VALUE;
+    }
 
     public long R(int n) {
         int sum = 0;
-    	if (n < this.ics.length) {
-        	sum += ics[n]; // return the initial condition at n
-    	} else {
-    		
-    		// we need to find out what n is mod i
-    		for (int i=0; i!=this.coefs.length; i++) { 
-    			if (n%this.coefs.length==i) {
-    				// n is equivalent to i
-    				// now we just take the sum over the recursive terms
-    				int coef; long rterm;
-    				for (int j=0; j!=this.coefs[i].length; j++){
-    					coef  = this.coefs[i][j];
-    					rterm = R(n-1-j);
-    					sum += coef*rterm;
-    				}
-	        	}
-	        }
-    	}
-    	int modSum=sum%field;
-    	return modSum;
+        if (n < this.ics.length) {
+            sum += ics[n]; // return the initial condition at n
+        } else {
+
+            // we need to find out what n is mod i
+            for (int i = 0; i != this.coefs.length; i++) {
+                if (n % this.coefs.length == i) {
+                    // n is equivalent to i
+                    // now we just take the sum over the recursive terms
+                    int coef;
+                    long rterm;
+                    for (int j = 0; j != this.coefs[i].length; j++) {
+                        coef = this.coefs[i][j];
+                        rterm = R(n - 1 - j);
+                        sum += coef * rterm;
+                    }
+                }
+            }
+        }
+        int modSum = sum % field;
+        return modSum;
     }
-    
+
     public static void A068911() {
-		int[] A068911_ics = new int [] {1,2,4};
-		int[][] A068911_coefs = new int [][] {{1,1,0},{1,1,1}};
-		Recursion A068911 = new Recursion(A068911_ics, A068911_coefs);
-		for (int i=0; i!=25; i++){
-			System.out.println(A068911.R(i));
-		}
+        int[] A068911_ics = new int[] {
+            1,
+            2,
+            4
+        };
+        int[][] A068911_coefs = new int[][] {
+            {
+                1,
+                1,
+                0
+            }, {
+                1,
+                1,
+                1
+            }
+        };
+        Recursion A068911 = new Recursion(A068911_ics, A068911_coefs);
+        for (int i = 0; i != 25; i++) {
+            System.out.println(A068911.R(i));
+        }
     }
-    
+
     public static void A068911_fixed() {
-		int[] A068911_ics = new int [] {1,2};
-		int[][] A068911_coefs = new int [][] {{2,0},{1,1}};
-		Recursion A068911 = new Recursion(A068911_ics, A068911_coefs);
-		System.out.println("A068911_fixed -- 1,2");
-		System.out.print(String.format("Field=%s:\t", "R"));
-		for (int i=0; i!=25; i++){
-			System.out.print(A068911.R(i));
-			System.out.print("\t");
-		} System.out.print("\n");
+        int[] A068911_ics = new int[] {
+            1,
+            2
+        };
+        int[][] A068911_coefs = new int[][] {
+            {
+                2,
+                0
+            }, {
+                1,
+                1
+            }
+        };
+        Recursion A068911 = new Recursion(A068911_ics, A068911_coefs);
+        System.out.println("A068911_fixed -- 1,2");
+        System.out.print(String.format("Field=%s:\t", "R"));
+        for (int i = 0; i != 25; i++) {
+            System.out.print(A068911.R(i));
+            System.out.print("\t");
+        }
+        System.out.print("\n");
     }
-	
-	private static void fibpeltri() {
-		int[] fibpeltri_ics = new int [] {0,1,2};
-		int[][] fibpeltri_coefs = new int [][] {{1,1,1},{1,1,0},{2,1,0}};
-		Recursion fibpeltri = new Recursion(fibpeltri_ics, fibpeltri_coefs);
-		System.out.println("Fibonacci-Pell-Tribonacci -- 0,1,2");
-		System.out.print(String.format("Field=-:\t"));
-		for (int i=0; i!=25; i++){
-			System.out.print(fibpeltri.R(i));
-			System.out.print("\t");
-		} System.out.print("\n");
-	}
-    
-	private static void A111() {
-		int[] A111_ics = new int [] {0,1,2};
-		int[][] A111_coefs = new int [][] {{1,0,1},{0,1,1},{0,0,1}};
-		Recursion A111 = new Recursion(A111_ics, A111_coefs);
-		System.out.println("A111 -- 0,1,2");
-		System.out.print(String.format("Field=-:\t"));
-		for (int i=0; i!=25; i++){
-			System.out.print(A111.R(i));
-			System.out.print("\t");
-		} System.out.print("\n");
-	}
-	
-	private static void R(int[] ics, int[][] coefs, String name) {
-		int[] fields = new int[] {2,3,4,5,7,8,9,11,13,16,17,19,23,25,27,29};
-		System.out.println(String.format("%s", name));
-		for (int f = 0; f != fields.length; f++) {
-			int field = fields[f];
-			System.out.print(String.format("Field=%s:\t", Integer.toString(field)));
-			for (int i=0; i!=25; i++){
-				Recursion A111 = new Recursion(ics, coefs, field);
-				System.out.print(A111.R(i));
-				System.out.print("\t");
-			} System.out.print("\n");
-		}
-	}
-	
-	private static void print_n() {
-		System.out.print(String.format("Term #:\t\t", "-"));
-		for (int i=0; i!=25; i++){
-			System.out.print(i);
-			System.out.print("\t");
-		} System.out.print("\n");
-	}
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
 
-//		A111();		
-//		
+    private static void fibpeltri() {
+        int[] fibpeltri_ics = new int[] {
+            0,
+            1,
+            2
+        };
+        int[][] fibpeltri_coefs = new int[][] {
+            {
+                1,
+                1,
+                1
+            }, {
+                1,
+                1,
+                0
+            }, {
+                2,
+                1,
+                0
+            }
+        };
+        Recursion fibpeltri = new Recursion(fibpeltri_ics, fibpeltri_coefs);
+        System.out.println("Fibonacci-Pell-Tribonacci -- 0,1,2");
+        System.out.print(String.format("Field=-:\t"));
+        for (int i = 0; i != 25; i++) {
+            System.out.print(fibpeltri.R(i));
+            System.out.print("\t");
+        }
+        System.out.print("\n");
+    }
 
-		
-		print_n();
-		
-		// Fibonacci - Pell - Tribonacci
-		fibpeltri();
-		
-		// A068911_fixed Field
-//		A068911();
-		R(new int[] {1,2}, new int[][] {{2,0},{1,1}}, "A068911_fixed Field");
-		
-		// A111 Field
-		R(new int[] {0,1,2}, new int [][] {{1,0,1},{0,1,1},{0,0,1}}, "A111");
-	}
+    private static void A111() {
+        int[] A111_ics = new int[] {
+            0,
+            1,
+            2
+        };
+        int[][] A111_coefs = new int[][] {
+            {
+                1,
+                0,
+                1
+            }, {
+                0,
+                1,
+                1
+            }, {
+                0,
+                0,
+                1
+            }
+        };
+        Recursion A111 = new Recursion(A111_ics, A111_coefs);
+        System.out.println("A111 -- 0,1,2");
+        System.out.print(String.format("Field=-:\t"));
+        for (int i = 0; i != 25; i++) {
+            System.out.print(A111.R(i));
+            System.out.print("\t");
+        }
+        System.out.print("\n");
+    }
+
+    private static void R(int[] ics, int[][] coefs, String name) {
+        int[] fields = new int[] {
+            2,
+            3,
+            4,
+            5,
+            7,
+            8,
+            9,
+            11,
+            13,
+            16,
+            17,
+            19,
+            23,
+            25,
+            27,
+            29
+        };
+        System.out.println(String.format("%s", name));
+        for (int f = 0; f != fields.length; f++) {
+            int field = fields[f];
+            System.out.print(String.format("Field=%s:\t", Integer.toString(field)));
+            for (int i = 0; i != 25; i++) {
+                Recursion A111 = new Recursion(ics, coefs, field);
+                System.out.print(A111.R(i));
+                System.out.print("\t");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    private static void print_n() {
+        System.out.print(String.format("Term #:\t\t", "-"));
+        for (int i = 0; i != 25; i++) {
+            System.out.print(i);
+            System.out.print("\t");
+        }
+        System.out.print("\n");
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        //		A111();
+        //
+
+        print_n();
+
+        // Fibonacci - Pell - Tribonacci
+        fibpeltri();
+
+        // A068911_fixed Field
+        //		A068911();
+        R(new int[] {
+            1,
+            2
+        }, new int[][] {
+            {
+                2,
+                0
+            }, {
+                1,
+                1
+            }
+        }, "A068911_fixed Field");
+
+        // A111 Field
+        R(new int[] {
+            0,
+            1,
+            2
+        }, new int[][] {
+            {
+                1,
+                0,
+                1
+            }, {
+                0,
+                1,
+                1
+            }, {
+                0,
+                0,
+                1
+            }
+        }, "A111");
+    }
 }
--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 [![Build Status](https://travis-ci.com/robert-7/math-assignments-and-presentations.svg?branch=master)](https://travis-ci.com/robert-7/math-assignments-and-presentations)
 
 Various papers and presentations done in LaTeX. Links to compiled PDFs can be found in the READMEs.
+
+## Contributing
+
+### Set up local linting
+
+Linting is done with [`pre-commit`](https://pre-commit.com), a multi-language package manager for pre-commit hooks.
+
+Follow the instructions at <https://pre-commit.com/#install> to install it, then run `pre-commit install` in the repo root to install the git hooks locally. They will run **automatically** every time you commit.
+
+If you want to run all the checks manually, use `pre-commit run --all-files`. If you want to run just one of the checks, use `pre-commit run <hook_id>`.
+
+All the hooks are also run automatically via CI when pushing, so installing them locally will save you from **realizing you made a mistake only after pushing**.
+
+#### Installing ChkTeX
+
+On Ubuntu, run this command:
+
+```shell
+sudo apt-get update && sudo apt-get install -y chktex
+```

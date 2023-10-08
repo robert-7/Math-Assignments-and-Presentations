fc7697f18eff132898e5ece57a79587cfbe3357c
Robert Lech <robert.lech123@gmail.com>
2023-10-07 23:54:11 -0400

    feat: uploads to specific directory in Google Drive [#41] (#43)

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,11 @@ sudo apt install -y chktex shellcheck texlive-full
 
 # install gdrive
 URL_TO_GDRIVE_BINARY="https://github.com/glotlabs/gdrive/releases/download/3.9.0/gdrive_linux-x64.tar.gz"
-wget "${URL_TO_GDRIVE_BINARY}" -O gdrive
-tar -xzf gdrive
-chmod +x gdrive
-sudo cp gdrive /usr/local/bin
+DOWNLOAD_FILE="gdrive"
+INSTALL_PATH="/usr/local/bin/gdrive"
+wget "${URL_TO_GDRIVE_BINARY}" -O "${DOWNLOAD_FILE}"
+tar -xzf "${DOWNLOAD_FILE}"
+chmod +x "${DOWNLOAD_FILE}"
+sudo cp "${DOWNLOAD_FILE}" "${INSTALL_PATH}"
+command -v "${DOWNLOAD_FILE}" | grep "${INSTALL_PATH}"
+rm -f "${DOWNLOAD_FILE}"

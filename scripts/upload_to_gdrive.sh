99369e2ababb20b894e83d8e3f8bd13d70feb9cb
Robert Lech <robert.lech123@gmail.com>
2023-10-07 23:52:59 -0400

    feat: uploads to specific directory in Google Drive [#41]

--- scripts/upload_to_gdrive.sh
+++ scripts/upload_to_gdrive.sh
@@ -21,6 +21,7 @@ push_to_gdrive() {
 
   if [ "${check}" -gt 1 ]; then
     echo "Too many instances. Exiting early..."
+    return
   fi
 
   source_dir="$(dirname "${source_path}")"
@@ -32,16 +33,23 @@ push_to_gdrive() {
     samefileID=$(gdrive files list --query "name = '${destination_path}'" --skip-header | sed 's/|/ /' | awk '{print $1}' | head -n 1)
     echo "Fetched the file ID. It's ${samefileID}."
     gdrive files update "${samefileID}" "${source_path_to_upload}"
+    echo "Done updating."
   else
     echo "It doesn't exist. We'll upload the file."
-    gdrive files upload "${source_path_to_upload}"
+    gdrive files upload --parent "${GDRIVE_DIRECTORY_ID}" "${source_path_to_upload}"
+    echo "Done uploading."
   fi
 
   rm -f "${source_path_to_upload}"
-  echo "Done uploading."
   echo
 }
 
-for source_path in "${source_paths[@]}"; do
-  push_to_gdrive "${source_path}"
-done
+# NOTE: This is the ID of the "Math Assignments and Presentations" directory. Verified below before continuing.
+GDRIVE_DIRECTORY_ID="1hHjYI4HzHuml5mo9G2RKdZnM2xv3s9mu"
+if gdrive files info "${GDRIVE_DIRECTORY_ID}" | grep "Math Assignments and Presentations"; then
+  for source_path in "${source_paths[@]}"; do
+    push_to_gdrive "${source_path}"
+  done
+else
+  echo "Can't verify that ${GDRIVE_DIRECTORY_ID} is the correct ID for our upload path. Exiting early..."
+fi
